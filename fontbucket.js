9082bf0e84adc0c101516a931720434bd107d4ff
rishabhscross <parammittal16@gmail.com>
2019-12-01 12:56:04 +0530

    Add js functions

---
+++ fontbucket.js
@@ -0,0 +1,47 @@
+let allFonts = [];
+window.onload = getFonts();
+
+function getFonts(){
+    chrome.storage.sync.get(['allFonts'],function (result){
+       console.log(result['allFonts']);
+       allFonts = result['allFonts'];
+       console.log('1', allFonts);
+       setOptions();
+    });
+};
+
+document.getElementById("addFontButton").onclick = postFonts;
+document.getElementById("applyFontButton").onclick = applyFont;
+
+function postFonts() {
+    let url = document.getElementById('fontUrl').value;
+    
+    let newList = allFonts;
+    newList.push(url);
+    console.log('2', newList);
+    chrome.storage.sync.set({'allFonts':newList}, function() {
+        console.log('Value is set to ' + newList);
+        getFonts();
+    });
+}
+
+function setOptions() {
+
+    let fontDropdown = document.getElementById('fonts');
+    while (fontDropdown.firstChild) {
+        fontDropdown.removeChild(fontDropdown.firstChild);
+    }
+    allFonts.forEach(function (item) {
+        let option = document.createElement("option");
+        option.text = item;
+        fontDropdown.add(option);
+    });
+}
+
+function applyFont() {
+    let selectedFont = document.getElementById('fonts').value;
+    console.log(selectedFont);
+    let option = document.createElement("link");
+
+
+}
