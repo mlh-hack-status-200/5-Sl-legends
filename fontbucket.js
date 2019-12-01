e0d983765deed15064f7dc111a368bdb9536c04b
rishabhscross <parammittal16@gmail.com>
2019-12-01 14:57:28 +0530

    UI completed

--- fontbucket.js
+++ fontbucket.js
@@ -1,7 +1,7 @@
 let allFonts = [];
 window.onload = getFonts();
 
-function getFonts(){
+ function getFonts(){
     chrome.storage.sync.get(['allFonts'],function (result){
        console.log(result['allFonts']);
        allFonts = result['allFonts'];
@@ -15,7 +15,8 @@ document.getElementById("applyFontButton").onclick = applyFont;
 
 function postFonts() {
     let url = document.getElementById('fontUrl').value;
-    
+    if(url.length !=0)
+    {
     let newList = allFonts;
     newList.push(url);
     console.log('2', newList);
@@ -23,6 +24,12 @@ function postFonts() {
         console.log('Value is set to ' + newList);
         getFonts();
     });
+    document.getElementById("fontUrl").value="";
+
+    }
+    else{
+        document.getElementById("err").innerHTML="Please add a valid url";
+    }
 }
 
 function setOptions() {
@@ -31,17 +38,29 @@ function setOptions() {
     while (fontDropdown.firstChild) {
         fontDropdown.removeChild(fontDropdown.firstChild);
     }
+    // setting default dropdown
+    let option = document.createElement("option");
+        option.text = "Select your font";
+        option.disabled = true;
+        option.selected  = true;
+        fontDropdown.add(option);
+        //
+        // adding the fonts to the dropdown
     allFonts.forEach(function (item) {
         let option = document.createElement("option");
         option.text = item;
         fontDropdown.add(option);
     });
+    //
 }
-
+// Applying font.
 function applyFont() {
     let selectedFont = document.getElementById('fonts').value;
-    console.log(selectedFont);
-    let option = document.createElement("link");
-
-
+    let link = document.createElement("link");
+    link.setAttribute("href", selectedFont);
+    link.setAttribute("rel", "stylesheet");
+    console.log(link);
+    chrome.tabs.executeScript({
+        code: "document.body.style.fontFamily = 'Poppins', sans-serif !important;"
+    });
 }
